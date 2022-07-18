4760002a3c98f51bb50f13367e5bd4d9446dc21f
BleakHeart <cosmin.marin19@gmail.com>
2022-07-18 17:16:49 +0200

    Added new function to automate magnetic field res

--- lib/frustration_graph.py
+++ lib/frustration_graph.py
@@ -5,7 +5,7 @@ import dimod
 import time
 
 
-def bqm_frustration(L: int, const:  float) -> dimod.BinaryQuadraticModel:
+def bqm_frustration(L: int, const:  float, h: float =0.0) -> dimod.BinaryQuadraticModel:
     """Function used to build the model described in Park and Lee's article[1]
     
     [1] Park, Hayun and Lee, Hunpyo. "Phase transition of Frustrated Ising model 
@@ -24,7 +24,6 @@ def bqm_frustration(L: int, const:  float) -> dimod.BinaryQuadraticModel:
     Lattice_Size = (L, L)
     C_G = nx.grid_graph(dim=Lattice_Size, periodic=False)
     
-    h = 0.0
     J1 = -1
     J2 = const * np.abs(J1)
 
@@ -65,11 +64,11 @@ def bqm_frustration(L: int, const:  float) -> dimod.BinaryQuadraticModel:
     return bqm
 
 
-def phase_transition(L, ratios, sampler, num_reads=100):
+def phase_transition(L: int, ratios: np.ndarray, sampler, num_reads: int =100, h: int =0.0):
     results = []
     i = 1
     for const in tqdm(ratios):
-        bqm = bqm_frustration(L, const)
+        bqm = bqm_frustration(L, const, h)
         sampleset = sampler.sample(bqm, num_reads=num_reads, 
                                    label=f'Ising Frustrated {i}/{ratios.size}') #chain_strenght=5
         i += 1
@@ -93,3 +92,35 @@ def phase_transition(L, ratios, sampler, num_reads=100):
         Energies.append(E)
     
     return Magnetizations, Frequencies, Energies
+
+
+def results_analysis(M: list, F: list, E: list):
+    n = len(M)
+
+    Mag, Chi, En = [], [], []
+
+    for i in range(n):
+        Magn = np.array(M[i])
+        Freq = np.array(F[i])
+        En = np.array(E[i])
+        
+        M_mean = (Magn * Freq).sum() / Freq.sum()
+        En_mean = (En * Freq).sum() / Freq.sum()
+        C = ((Magn - M_mean)**2 * Freq).sum() / Freq.sum()
+        Mag.append(M_mean)
+        Chi.append(C)
+        En.append(En_mean)
+    
+    return Mag, Chi, En
+
+
+def h_transition(L: int, ratios: np.ndarray, H: np.ndarray, sampler, num_reads=100):
+    Ms, Cs, Es = [], [], []
+    for h in H:
+        print(f"Computing Phase Transition for B={h}")
+        M, F, E = phase_transition(L, ratios, sampler, num_reads, h)
+        M, C, E = results_analysis(M, F, E)
+
+        Ms.append(M), Cs.append(C), Es.append(E)
+    
+    return Ms, Cs, Es
