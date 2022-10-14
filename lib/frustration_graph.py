1e0b7f64c86a38d17e2b73532d2d5d9256bafd85
Wronsmin <cosmin.marin19@gmail.com>
2022-10-14 11:11:55 +0200

    Finished the simulations on cpu

--- lib/frustration_graph.py
+++ lib/frustration_graph.py
@@ -3,7 +3,8 @@ import numpy as np
 from tqdm import tqdm
 import dimod
 import time
-import graphtool as gt 
+import graph_tool.all as gt 
+import matplotlib.pyplot as plt
 
 
 def bqm_frustration(L: int, const:  float, h: float =0.0) -> dimod.BinaryQuadraticModel:
@@ -121,9 +122,100 @@ def h_transition(L: int, ratios: np.ndarray, H: np.ndarray, sampler, num_reads=1
     return Ms, Cs, Es
 
 
-def cpu_sim(L: int, T: np.ndarray, ratios: np.ndarray, H: np.ndarray, periodic: bool=False, num_sim=100):
-    lattice = gt.generation.lattice(shape=[L, L], periodic=periodic)
+class FIM_cpu():
+    def __init__(self, L: int, T: float, ratios: np.ndarray, 
+                 H: float, periodic: bool=False, num_sim=100) -> None:
+        self.L = L
+        self.T = T
+        self.ratios = ratios
+        self.H = H
+        self.periodic = periodic
+        
+        self.num_sim = num_sim
+        self.epochs = 2000
+        
+        
+    def _graph_building(self, ratio, periodic):
+        g = gt.lattice([self.L, self.L], periodic=periodic)
+        
+        J1 = 1
+        J2 = -(ratio * J1)
+        
+        edges = [(*edge, J1) for edge in g.get_edges().tolist()]
+
+        indices = g.get_vertices().reshape([self.L, self.L])
+        for x in range(self.L-1):
+            for y in range(self.L-1):
+                # diagonals on right
+                edges.append((*indices[[x, x+1], [y, y+1]], J2))
+                edges.append((*indices[[x+1, x], [y, y+1]], J2))
+        
+        
+        g = gt.Graph(directed=False)
+        self.weights = g.new_ep("double")
+
+        g.add_edge_list(edges, eprops=[self.weights])
+        
+        pos = []
+        for x in range(self.L):
+            for y in range(self.L):
+                pos.append([x, y])
+                
+        pos = np.array(pos)
+        self.pos = g.new_vertex_property("vector<double>", pos)
+        
+        return g
     
     
+    def sim_run(self):
+        
+        self.M = np.zeros([self.num_sim, len(self.ratios)])
+        self.X = np.zeros([self.num_sim, len(self.ratios)])
+        
     
-    
+        for ratio in tqdm(self.ratios): # transition loop
+            self.G = self._graph_building(ratio, self.periodic)
+            
+            num_nodes = self.G.num_vertices()
+            Mag = np.zeros(self.num_sim)
+            X = np.zeros(self.num_sim)
+            
+            for i in range(self.num_sim):
+                M = []
+                state = gt.IsingMetropolisState(self.G, beta=1/self.T, w=self.weights)
+                state.iterate_async(niter=100 * num_nodes)
+                for e in range(self.epochs):
+                    state.iterate_async(niter=num_nodes)
+                    M.append(np.abs(state.s.fa.mean()))
+                
+                Mag[i] = np.mean(M)
+                X[i] = np.var(M)
+            
+            idx = np.where(self.ratios == ratio)[0][0]
+            
+            self.M[:, idx] = Mag[:]
+            self.X[:, idx] = X[:] 
+            
+    def plot_results(self):
+        mean = self.M.mean(axis=0)
+        std = self.M.std(axis=0)
+        
+        plt.plot(self.ratios, mean, label='B=0')
+        plt.fill_between(self.ratios, mean - std, mean + std, alpha=0.5)
+        plt.grid()
+        plt.xlabel(r'$J_2$/$J_1$', fontsize=18)
+        plt.ylabel('|M| [arb. unit]', fontsize=18)
+        plt.tight_layout()
+        plt.legend()
+        
+        mean = self.X.mean(axis=0)
+        std = self.X.std(axis=0)
+        
+        plt.plot(self.ratios, mean, label='B=0')
+        plt.fill_between(self.ratios, mean - std, mean + std, alpha=0.5)
+        plt.grid()
+        plt.xlabel(r'$J_2$/$J_1$', fontsize=18)
+        plt.ylabel('X [arb. unit]', fontsize=18)
+        plt.tight_layout()
+        plt.legend()
+                
